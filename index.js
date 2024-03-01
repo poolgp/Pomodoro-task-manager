831665b4d286b415dcb5ebd357e86e91c4ec31b8
poolgp <145465364+poolgp@users.noreply.github.com>
2024-03-01 18:05:21 +0100

    01/03/2024

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// function newTasca() {
-//   Swal.fire({
-//     title: "Create New Task",
-//     html:
-//       '<input id="task-name" class="swal2-input" placeholder="Task Name">' +
-//       '<textarea id="task-description" class="swal2-textarea" placeholder="Task Description"></textarea>',
-//     showCancelButton: true,
-//     confirmButtonText: "Create Task",
-//     cancelButtonText: "Cancel",
-//     focusConfirm: false,
-//     preConfirm: () => {
-//       const taskName = Swal.getPopup().querySelector("#task-name").value;
-//       const taskDescription =
-//         Swal.getPopup().querySelector("#task-description").value;
-//       console.log("Task Name:", taskName);
-//       console.log("Task Description:", taskDescription);
-//     },
-//   });
-// }
-
 function newTasca() {
   Swal.fire({
     title: "Create New Task",
@@ -35,24 +15,27 @@ function newTasca() {
       console.log("Task Name:", taskName);
       console.log("Task Description:", taskDescription);
 
-      // Crear un nuevo elemento div para representar la tarea
       const newTaskElement = document.createElement("div");
       newTaskElement.classList.add("task");
 
-      // Asignar el nombre y la descripción como contenido del nuevo elemento
       newTaskElement.innerHTML = `
-      <h4 class="taskH4">${taskName}</h4>
-      <p class="taskP">${taskDescription}</p>
-      <button
-      <hr/>
-
+      <h4 class = "taskH4">${taskName}</h4>
+      <p class = "taskP">${taskDescription}</p>
+      <br/>
+      <button class="taskInfo">
+      <i class="fa-solid fa-circle-info"></i>
+      </button>
+      <button class="taskDelete">
+      <i class="fa-solid fa-trash-can"></i>
+      </button>
       `;
 
-      
+      guardarLocalString();
 
-      // Agregar la tarea al contenedor de tareas pendientes
       const pendentTasksContainer = document.getElementById("tasquesPendents");
       pendentTasksContainer.appendChild(newTaskElement);
     },
   });
 }
+
+guardarLocalString();
